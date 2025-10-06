51fc9812c744c6b5b84eb3eced57a09b554dfbfe
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2025-10-06 20:52:09 +0800

    setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+const app = express();
+const PORT = 5000; 
+
+
+app.use(express.json());
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
